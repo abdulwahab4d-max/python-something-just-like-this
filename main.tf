1821e2b8df929be3f545c125f69c6970f5b68898
abdulwahab4d-max <abdul.wahab.4d@gmaiil.com>
2025-12-05 22:56:11 +0500

    running code on EC2

---
+++ main.tf
@@ -0,0 +1,65 @@
+# Create or import your SSH key pair
+resource "aws_key_pair" "mykey" {
+  key_name   = "mykey"
+  public_key = var.ssh_public_key   # taking ssh key fro repository secret
+}
+
+# Security group for HTTP + SSH
+resource "aws_security_group" "allow_http" {
+  name_prefix = "allow_http_"
+
+#port enabled for public ip
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "ubuntu" {
+  ami           = "ami-0fa91bc90632c73c9"
+  instance_type = "t3.micro"
+
+  # 🟢 Add the SSH key here
+  key_name = aws_key_pair.mykey.key_name
+
+  # Attach the security group
+  vpc_security_group_ids = [aws_security_group.allow_http.id]
+
+  tags = {
+    Name = "Terraform-Ubuntu-python-something-just-like-this"
+  }
+
+  user_data = <<-EOF
+    #!/bin/bash
+    apt update -y
+    apt install -y docker.io
+    
+    # Enable docker
+    systemctl start docker
+    systemctl enable docker
+    
+    sudo apt update
+    sudo apt install python3
+    sudo apt install python3-pip
+    sudo apt install python3-venv python3-dev build-essential
+    python3 -m venv myenv
+    python3 lyrics.py
+    source myenv/bin/activate
+  EOF
+}
---
+++ providers.tf
@@ -0,0 +1,25 @@
+# providers.tf
+
+provider "aws" {
+  region = "eu-north-1"  # Change to your desired region
+}
+
+terraform {
+  backend "s3" {
+    bucket = "terraform-bucket-aw123"
+    key = "state/terraform.tfstate"
+    region = "eu-north-1"
+  }
+}
+
+# Ip of the Instance created by teraform
+output "ec2_public_ip" {
+  value = aws_instance.ubuntu.public_ip
+}
+
+variable "ssh_public_key" {
+  type      = string
+  sensitive = true
+}
+
+
