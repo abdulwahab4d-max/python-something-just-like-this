1fc19a851497dd670ccb91607dd2e8bb447cded1
abdulwahab4d-max <abdul.wahab.4d@gmail.com>
2025-12-07 00:52:44 +0500

    testing container with multiple projects

--- providers.tf
+++ providers.tf
@@ -8,7 +8,7 @@ provider "aws" {
 terraform {
   backend "s3" {
     bucket = "terraform-bucket-aw123"
-    key = "python-something-just-like-this/terraform.tfstate"
+    key = "something-just-like-this/terraform.tfstate"
     region = "eu-north-1"
   }
 }
